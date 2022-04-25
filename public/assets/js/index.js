e5944388605cb2776dbbef5c88a5cc6144db908d
euc1 <jamesharrington585@hotmail.com>
2022-04-25 17:24:25 -0400

    test

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -29,7 +29,6 @@ const getNotes = () =>
   fetch('/api/notes', {
     method: 'GET',
     headers: {
-      Accept: 'application/json',
       'Content-Type': 'application/json',
     },
   });
@@ -38,7 +37,6 @@ const saveNote = (note) =>
   fetch('/api/notes', {
     method: 'POST',
     headers: {
-      Accept: 'application/json',
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
