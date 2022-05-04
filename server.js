5cfe2aaa362043ff257fb9cf353bbb5c2d59c4cf
euc1 <jamesharrington585@hotmail.com>
2022-05-04 18:13:03 -0400

    check to see if anything wasn't pushed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
